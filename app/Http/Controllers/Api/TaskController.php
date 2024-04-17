45e16f7d0eded539c0ec0cceb14ba081247fd3c8
mvalenciaz <mvalenciaz@misena.edu.co>
2024-04-16 19:21:22 -0500

    creando api rest

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json([
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $task = new Task();
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->finished = $request->finished;
+
+        $task->save();
+
+        return response()->json([
+            'task' => $task
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        return response()->json([
+            'task' => $task 
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->finished = $request->finished;
+
+        $task->save();
+
+        return response()->json([
+            'task' => $task
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $task->delete();
+
+        return response()->json([
+            'task' => $task
+        ]);
+    }
+}
